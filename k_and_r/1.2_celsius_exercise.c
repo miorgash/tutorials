097cf595e3c3dc5885e0b34ffc27deb780a79b06
五十嵐海織 <miori@igarashikaishikinoMacBook-Air.local>
2021-12-19 09:12:27 +0900

    1.2 done

---
+++ k_and_r/1.2_celsius_exercise.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* celsius=0, ..., 100 に対して、摂氏-華氏対応表
+   を印字する */
+main()
+{
+    printf("fahr=0.20, ..., 2300 に対して、華氏-摂氏対応表を印字する\n");
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* 温度表の下限 */
+    upper = 100;    /* 上限 */
+    step  = 10;     /* きざみ */
+
+    celsius = lower;
+    while (celsius <= upper) {
+        fahr = (9.0/5.0) * celsius + 32.0;
+        printf("%3.0f\t%6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
--- k_and_r/1.2_celsius_float.c
+++ k_and_r/1.2_celsius_float.c
@@ -4,7 +4,7 @@
    を印字する */
 main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;      /* 温度表の下限 */
@@ -13,8 +13,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%3d\t%6d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
