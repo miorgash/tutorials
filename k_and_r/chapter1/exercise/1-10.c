a581b8236175199a9e92d2f7e14a16ec539cf148
五十嵐海織 <miori@igarashikaishikinoMacBook-Air.local>
2021-12-27 11:07:09 +0900

    I think it works

---
+++ k_and_r/chapter1/exercise/1-10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main ()
+{
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+            putchar("\\t");
+        else if (c == '\b')
+            putchar("\\b");
+        else if (c == '\\')
+            putchar("\\");
+        else
+            putchar(c);
+    }    
+}
